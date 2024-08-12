2fdb1fc211c2db0620e7a150b95d39d89f41e270
Joza <mehojoey@gmail.com>
2024-08-12 15:03:53 +0300

    add content-type json

--- src/main/java/api/ConnectionUtility.java
+++ src/main/java/api/ConnectionUtility.java
@@ -6,14 +6,17 @@ import io.restassured.response.Response;
 public class ConnectionUtility {
 
     public static Response makeGetRequestToUriAndReturnResponse(Endpoints endpoint) {
-        return RestAssured.given()
-                .header("Accept", "application/json")
+        Response response = RestAssured.given()
+//                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
                 .when()
                 .get(endpoint.getUri())
                 .then()
                 .statusCode(200)
                 .extract()
                 .response();
+        System.out.println(response.getBody().asString());
+        return response;
     }
 
 }
